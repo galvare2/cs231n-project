4ae34325e26226fef59432dd596922766b87de3f
taoketao <morgan.r.bryant@gmail.com>
2016-03-01 19:18:54 -0800

    tryna commit

--- train_net.py
+++ train_net.py
@@ -12,7 +12,7 @@ IMAGE_HEIGHT = 128
 IMAGE_WIDTH = 128
 
 def train(data):
-
+    pass
 
 '''
 load_data:
@@ -22,33 +22,33 @@ This will include augmented data.
 '''
 def load_data():
 
-	# Read each image
-	data = {}
-	X_all = []
-	y_all = []
-	for i_1 in range(3):
-		for i_2 in range(3):
-			for i_3 in range(3):
-				for i_4 in range(3):
-					(img, labels) = load_image(i_1, i_2, i_3, i_4, augment=True)
-					X_all.extend(imgs)
-					y_all.extend(labels)
-	
-    # Shuffle X_all, y_all in unison to separate similar/augmented data points
+    # Read each image
+    data = {}
+    X_all = []
+    y_all = []
+    for i_1 in range(3):
+        for i_2 in range(3):
+            for i_3 in range(3):
+                for i_4 in range(3):
+                    (img, labels) = load_image(i_1, i_2, i_3, i_4, augment=True)
+                    X_all.extend(imgs)
+                    y_all.extend(labels)
+                    # Shuffle X_all, y_all in unison to separate similar/augmented data points
+    print len(X_all)
     p = np.random.permutation(len(X_all))
     X_all = X_all[p]
     y_all = y_all[p]
 
     # Apply cutoffs to separate out the data
-	train_cutoff = int(len(X_all) * (1 - TEST_DATA_RATIO - VAL_DATA_RATIO)) # Apply cutoff
-	val_cutoff = int(len(X_all) * (1 - TEST_DATA_RATIO))
-	data['X_train'] = X_all[:train_cutoff]
-	data['y_train'] = y_all[:train_cutoff]
-	data['X_val'] = X_all[:val_cutoff]
-	data['y_val'] = y_all[:val_cutoff]
-	data['X_test'] = X_all[val_cutoff:]
-	data['y_test'] = y_all[val_cutoff:]
-	return data
+    train_cutoff = int(len(X_all) * (1 - TEST_DATA_RATIO - VAL_DATA_RATIO)) # Apply cutoff
+    val_cutoff = int(len(X_all) * (1 - TEST_DATA_RATIO))
+    data['X_train'] = X_all[:train_cutoff]
+    data['y_train'] = y_all[:train_cutoff]
+    data['X_val'] = X_all[:val_cutoff]
+    data['y_val'] = y_all[:val_cutoff]
+    data['X_test'] = X_all[val_cutoff:]
+    data['y_test'] = y_all[val_cutoff:]
+    return data
 
 '''
 load_image: Loads an image corresponding to the given label indices. Returns a tuple
@@ -57,33 +57,33 @@ if augment is set to true.
 Rotates images to have the same orientation and then scales them to a set height and width.
 '''
 def load_image(i_1, i_2, i_3, i_4, augment=True):
-	
-	# Read image from file. Not included in github repo, must be downloaded from drive.
-	img_string = DATA_LOCATION + str(i_1) + str(i_2) + str(i_3) + str(i_4) + DATA_EXTENSION
-	img = ndimage.imread(img_string)
+    
+    # Read image from file. Not included in github repo, must be downloaded from drive.
+    img_string = DATA_LOCATION + str(i_1) + str(i_2) + str(i_3) + str(i_4) + DATA_EXTENSION
+    img = ndimage.imread(img_string)
 
-	# Rotate so all images have portrait orientation
-	if img.shape[1] > img.shape[0]:
-		img = np.swapaxes(img, 0, 1)
+    # Rotate so all images have portrait orientation
+    if img.shape[1] > img.shape[0]:
+        img = np.swapaxes(img, 0, 1)
 
-	# Make all images the same, manageable size
-	img = misc.imresize(img, (IMAGE_HEIGHT, IMAGE_WIDTH), interp='nearest')	
+    # Make all images the same, manageable size
+    img = misc.imresize(img, (IMAGE_HEIGHT, IMAGE_WIDTH), interp='nearest') 
 
 
-	# Make labels. 
-	# IMPORTANT NOTE: I'm putting the labels as numpy vectors. We can add more/less structure
-	# to this such as changing it to a dictionary if it seems necessary.
-	label = (i_1, i_2, i_3, i_4)
-	label = np.asarray(label)
-	
-	# Apply augmentations
-	if augment:
-		imgs = apply_augmentations(img)
-		labels = list(itertools.repeat(label, len(imgs)))
-	else:
-		imgs = [img]
-		labels = [label]
-	return (imgs, labels)
+    # Make labels. 
+    # IMPORTANT NOTE: I'm putting the labels as numpy vectors. We can add more/less structure
+    # to this such as changing it to a dictionary if it seems necessary.
+    label = (i_1, i_2, i_3, i_4)
+    label = np.asarray(label)
+    
+    # Apply augmentations
+    if augment:
+        imgs = apply_augmentations(img)
+        labels = list(itertools.repeat(label, len(imgs)))
+    else:
+        imgs = [img]
+        labels = [label]
+    return (imgs, labels)
 
 '''
 apply_augmentations: from an image, returns a list of images which are augmented versions of that
@@ -95,7 +95,7 @@ image. The following augmentations are applied:
 '''
 
 def apply_augmentations(img):
-	return [img]
+    return [img]
 
 
 '''
@@ -107,8 +107,8 @@ model.add(Dense(output_dim=10, init="glorot_uniform"))
 model.add(Activation("softmax")) '''
 
 def main():
-	data = load_data()
-	train(data)
+    data = load_data()
+    train(data)
 
 if __name__ == "__main__":
     main()
