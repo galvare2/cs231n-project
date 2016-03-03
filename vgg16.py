455815b334aab581fc5b80ed09ef97501be808dd
Gabe Alvarez <gabealvarez@DN51qo4h.SUNet>
2016-03-02 19:19:52 -0800

    all pretrained weights

--- vgg16.py
+++ vgg16.py
@@ -25,40 +25,55 @@ def build_model(input_var, BATCH_SIZE=None):
             W = w[0], b = w[1])
     print "hi"
     net['conv1_2'] = ConvLayer(
-            net['conv1_1'], 64, 3, pad=1, flip_filters=False)
+            net['conv1_1'], 64, 3, pad=1, flip_filters=False,
+            W = w[2], b = w[3])
     net['pool1'] = PoolLayer(net['conv1_2'], 2)
     net['conv2_1'] = ConvLayer(
-            net['pool1'], 128, 3, pad=1, flip_filters=False)
+            net['pool1'], 128, 3, pad=1, flip_filters=False,
+            W = w[4], b = w[5])
     net['conv2_2'] = ConvLayer(
-            net['conv2_1'], 128, 3, pad=1, flip_filters=False)
+            net['conv2_1'], 128, 3, pad=1, flip_filters=False,
+            W = w[6], b = w[7])
     net['pool2'] = PoolLayer(net['conv2_2'], 2)
     net['conv3_1'] = ConvLayer(
-            net['pool2'], 256, 3, pad=1, flip_filters=False)
+            net['pool2'], 256, 3, pad=1, flip_filters=False,
+            W = w[8], b = w[9])
     net['conv3_2'] = ConvLayer(
-            net['conv3_1'], 256, 3, pad=1, flip_filters=False)
+            net['conv3_1'], 256, 3, pad=1, flip_filters=False,
+            W = w[10], b = w[11])
     net['conv3_3'] = ConvLayer(
-            net['conv3_2'], 256, 3, pad=1, flip_filters=False)
+            net['conv3_2'], 256, 3, pad=1, flip_filters=False,
+            W = w[12], b = w[13])
     net['pool3'] = PoolLayer(net['conv3_3'], 2)
     net['conv4_1'] = ConvLayer(
-            net['pool3'], 512, 3, pad=1, flip_filters=False)
+            net['pool3'], 512, 3, pad=1, flip_filters=False,
+            W = w[14], b = w[15])
     net['conv4_2'] = ConvLayer(
-            net['conv4_1'], 512, 3, pad=1, flip_filters=False)
+            net['conv4_1'], 512, 3, pad=1, flip_filters=False,
+            W = w[16], b = w[17])
     net['conv4_3'] = ConvLayer(
-            net['conv4_2'], 512, 3, pad=1, flip_filters=False)
+            net['conv4_2'], 512, 3, pad=1, flip_filters=False,
+            W = w[18], b = w[19])
     net['pool4'] = PoolLayer(net['conv4_3'], 2)
     net['conv5_1'] = ConvLayer(
-            net['pool4'], 512, 3, pad=1, flip_filters=False)
+            net['pool4'], 512, 3, pad=1, flip_filters=False,
+            W = w[20], b = w[21])
     net['conv5_2'] = ConvLayer(
-            net['conv5_1'], 512, 3, pad=1, flip_filters=False)
+            net['conv5_1'], 512, 3, pad=1, flip_filters=False,
+            W = w[22], b = w[23])
     net['conv5_3'] = ConvLayer(
-            net['conv5_2'], 512, 3, pad=1, flip_filters=False)
+            net['conv5_2'], 512, 3, pad=1, flip_filters=False,
+            W = w[24], b = w[25])
     net['pool5'] = PoolLayer(net['conv5_3'], 2)
-    net['fc6'] = DenseLayer(net['pool5'], num_units=4096)
+    net['fc6'] = DenseLayer(net['pool5'], num_units=4096,
+            W = w[26], b = w[27])
     net['fc6_dropout'] = DropoutLayer(net['fc6'], p=0.5)
-    net['fc7'] = DenseLayer(net['fc6_dropout'], num_units=4096)
+    net['fc7'] = DenseLayer(net['fc6_dropout'], num_units=4096,
+            W = w[28], b = w[29])
     net['fc7_dropout'] = DropoutLayer(net['fc7'], p=0.5)
     net['fc8'] = DenseLayer(
-            net['fc7_dropout'], num_units=1000, nonlinearity=None)
+            net['fc7_dropout'], num_units=1000, nonlinearity=None,
+            W = w[30], b = w[31])
     net['prob'] = NonlinearityLayer(net['fc8'], softmax)
     
     return net#
